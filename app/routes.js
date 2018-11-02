df548e2d557ecee2e2344e95872a212697efee57
Tzikas <niko.tzikas@gmail.com>
2018-11-02 12:14:13 -0400

    no brain

--- app/routes.js
+++ app/routes.js
@@ -5,339 +5,28 @@ const Article= require('./models/article');
 var request = require('request');
 var cheerio = require('cheerio');
 
-const trainingData = [
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    },
-    {
-        input: "So true, thank you!",
-        output: { dislike: 1 }
-    },{
-        input: "Make America Great Again", 
-        output: { dislike: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { like: 1 }
-    }
-    
-    
-]
-
-let trainedNet;
-
-function encode(arg) {
-    return arg.split('').map(x => (x.charCodeAt(0) / 256));
-}
-
-function processTrainingData(data) {
-    return data.map(d => {
-
-        //console.log('d',d)
-        let output;
-        if(d.like==1){
-            output={like:1}
-        } else {
-            output={dislike:1}
-        }
-        //console.log( output,  encode(d.headerText) )
-        //console.log( d.output, encode(d.input) )
-        return {
-            input: encode(d.input),
-            output: d.output
-        }
-    })
-}
-
-    /*
-function train(data) {
-    let net = new brain.NeuralNetwork();
-    net.trainAsync(processTrainingData(data)).then(res=>{ console.log('traincallback is ',res)});
-    trainedNet = net.toFunction();
-    console.log('train')
-};
-
-*/
-
 
     
-function train(data) {
-    console.log('ho')
-    return new Promise(function(resolve, reject) {
-        let net = new brain.NeuralNetwork();
-        net.trainAsync(processTrainingData(data)).then(res=>{ 
-            console.log('traincallback', res)
-            resolve(res)
-        });
-        trainedNet = net.toFunction();
-        console.log('train')
-    })
-};
 
-
-
-
-
-
-/*
-var promise1 = new Promise(function(resolve, reject) {
-    console.log("promise1")
-    setTimeout(function() {
-    console.log("promise1 resolved")
-        
-    resolve('foo');
-  }, 10000);
-});*/
-
-
-
-
-
-
-function execute(input) {
-    let results = trainedNet(encode(input));
-    console.log('results ',results)
-    let output;
-    let certainty;
-    if (results.dislike > results.like) {
-        output = 'Donald dislike'
-        certainty = Math.floor(results.dislike * 100)
-    } else { 
-        output = 'Kim like'
-        certainty = Math.floor(results.like * 100)
-    }
-
-    return results
-}
-
-
-let statement=["Why I dyed my hair pink","Make America Great Again","'Congressman Schiff omitted and distorted key facts' @FoxNews  So, what else is new. He is a total phony!","Why I dyed my hair pink","Make America Great Again","'Congressman Schiff omitted and distorted key facts' @FoxNews  So, what else is new. He is a total phony!","Why I dyed my hair pink","Make America Great Again","'Congressman Schiff omitted and distorted key facts' @FoxNews  So, what else is new. He is a total phony!","Why I dyed my hair pink","Make America Great Again","'Congressman Schiff omitted and distorted key facts' @FoxNews  So, what else is new. He is a total phony!","Why I dyed my hair pink","Make America Great Again","'Congressman Schiff omitted and distorted key facts' @FoxNews  So, what else is new. He is a total phony!","Why I dyed my hair pink","Make America Great Again","'Congressman Schiff omitted and distorted key facts' @FoxNews  So, what else is new. He is a total phony!","Why I dyed my hair pink","Make America Great Again","'Congressman Schiff omitted and distorted key facts' @FoxNews  So, what else is new. He is a total phony!"]
 
 
 
 module.exports = function(app, passport) {
-    //app.get('/medium',isLoggedIn,async function(req,res){ 
 
-    app.get('/medium',async function(req,res){ 
+    app.get('/medium', isLoggedIn, async function(req,res){ 
         console.log(req.query, req.params);
         
         
         let articles=await Article.find({
-            //userId:req.user._id
+            userId:req.user._id
         }).exec();
-        console.log('ARTICLES', articles.length);
-        /*promise1.then(function(value) {
-            console.log(value);
-            // expected output: "foo"
-        });*/
-        /*  if(articles.length != 0){
-        
-             let t = await train(articles);
-            //let t = await train(articles);
-            console.log('t',t)
-
-            for(let i=0;i<statement.length;i++){
-                console.log(i)
-                //console.log(execute(statement[i]));
-            }
-
-   
-        }*/
-
-
-
+      
         request(`https://medium.com/search?q=${req.query.q}`, async function(err, resp, html) {
         console.log(err);
             if (!err){
                 const $ = cheerio.load(html);
                 
-                if(articles.length>0){
-
-                    let t = await train(articles);
-                    console.log('t',t)
-                    $('.section-inner').each(function(e){
-                        let text=$(this).find('h3').text();
-                        console.log("TEXT",text); 
-                        //execute(text)
-                        //console.log($(this).find('h3').text());
-                        let result=execute(text);
-                        console.log('result ',result)
-                    //let results={like:Math.random()}*/
-                        if(!isNaN(result.like)){
-                            //$(this).append(`<div>${Math.floor(results.like*100)}%</div>`);
-                            $(this).append(`<div>You like this ${(result.like).toFixed(2)*100}%</div>`);
-                        } else { 
-                            $(this).append(`<div class="calculation">No calculation available</div>`);
-                        }
-                        
-                    }); 
-                }                   
+                
                 let temp=$('.postArticle-content').append(`<br>
                 <button class="like-btn-js">Like me</button>
                 <button class="dislike-btn-js">Hate me</button>
@@ -347,7 +36,6 @@ module.exports = function(app, passport) {
                 
                 
                 $.html();
-                //console.log(section.html());
                 res.render('medium.hbs', {
                     html:section
                 });
